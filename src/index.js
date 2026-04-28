08f694b12ccb9470d13dbee7d844d1de2350e79e
Josephmtaku <115007021+Josephmtaku@users.noreply.github.com>
2026-04-28 13:13:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import App from './App';
 import AdminApp from './AdminApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 const isAdmin = window.location.pathname.startsWith('/admin');
-
 root.render(isAdmin ? <AdminApp /> : <App />);
