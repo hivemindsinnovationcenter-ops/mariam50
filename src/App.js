6394f0c5140bd97229d7b759ec63656097538848
Josephmtaku <115007021+Josephmtaku@users.noreply.github.com>
2026-04-29 10:00:38 +0100

    Fix formatting in generateUUID function

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ var DEADLINE      = new Date("2026-05-15T23:59:59");
 function generateUUID() {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0;
-    var v = c === "x" ? r : (r & 0x3 | 0x8);
+    var v = c === "x" ? r : ((r & 0x3) | 0x8);
     return v.toString(16);
   });
 }
